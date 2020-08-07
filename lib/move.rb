a2c204adb9da11a876e728f35d10c9ef952a885b
Adam Shilling <admshng@gmail.com>
2020-08-07 00:49:43 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,7 +14,7 @@ end
 
 def move(board, index, char="X")
    def update_array_at_with(array, index, value)
-  array[index] = value
+  board[index] = char
 end
   update_array_at_with(board, 0, "X")
 end 
